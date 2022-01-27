d3b8eca39920e35a2292e4e6a86cbfa1ee1b5d38
paultakem <chupaul92@yahoo.com>
2022-01-27 14:32:17 +0100

    implemented snake moving

--- js/snake.js
+++ js/snake.js
@@ -1,11 +1,14 @@
 const gameBoard = document.getElementById("gameCanvas");
 const gameContext = gameBoard.getContext("2d");
 
+let dx = 10;
+let dy = 0;
 
 let snake = [  {x: 250, y: 250}, 
                {x: 240, y: 250}, 
                {x: 230, y: 250},
             ];
+
 
 
 function drawSnakePart(part){
@@ -15,8 +18,38 @@ function drawSnakePart(part){
     gameContext.strokeRect(part.x, part.y, 10,10);
 }
 
+
+function clearCanvas() {
+      
+    gameContext.fillStyle = 'lightblue';
+    gameContext.strokestyle = 'black';
+    gameContext.fillRect(0, 0, 500, 500);
+     gameContext.strokeRect(0, 0, 500, 500);
+      }
+
 function drawSnake(){
     snake.forEach(drawSnakePart)
 }
 
-drawSnake()
+function moveSnake() 
+{  
+   const head = {x: snake[0].x + dx, y: snake[0].y + dy};
+  snake.unshift(head);
+  snake.pop();
+}
+
+function main() 
+{  
+   setTimeout(function onTick() 
+   {    
+     clearCanvas();    
+     moveSnake();  
+     drawSnake();
+     
+     main();
+   }, 100)
+}
+
+main()
+
+
