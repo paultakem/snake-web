8b3fe0d7efac563ea0831a63311efc3a394396fb
paultakem <chupaul92@yahoo.com>
2022-03-02 18:38:24 +0100

    Added resume game feature
    - Added feature to resume game after pause
    - Added credentials in footer

--- js/snake.js
+++ js/snake.js
@@ -3,6 +3,7 @@ const gameContext = gameBoard.getContext("2d");
 const scoreBoard = document.getElementById("scoreBoard");
 const scoreList = document.getElementById("highScoreList")
 const pauseBtn = document.getElementById("pauseBtn");
+const resumeBtn = document.getElementById("resumeBtn");
 const restartBtn = document.getElementById("restartBtn");
 
 
@@ -18,20 +19,41 @@ let highScoreArray = [];
 let paused = false;
 let over = false;
 
-pauseBtn.addEventListener('click', function(){ paused = !paused });
-restartBtn.addEventListener('click',  restart);
+pauseBtn.addEventListener('click', function () {
+  paused = !paused
+});
+restartBtn.addEventListener('click', restart);
+resumeBtn.addEventListener('click', resume);
 
 
-let snakeInnit = [  {x: gameBoard.width/2, y: gameBoard.height/2}, 
-  {x: gameBoard.width/2-10, y: gameBoard.height/2-10}, 
-  {x: gameBoard.width/2-20, y: gameBoard.width/2-20},
+let snakeInnit = [{
+    x: gameBoard.width / 2,
+    y: gameBoard.height / 2
+  },
+  {
+    x: gameBoard.width / 2 - 10,
+    y: gameBoard.height / 2 - 10
+  },
+  {
+    x: gameBoard.width / 2 - 20,
+    y: gameBoard.width / 2 - 20
+  },
 ];
 
 
-let snake = [  {x: gameBoard.width/2, y: gameBoard.height/2}, 
-               {x: gameBoard.width/2-10, y: gameBoard.height/2-10}, 
-               {x: gameBoard.width/2-20, y: gameBoard.width/2-20},
-            ];
+let snake = [{
+    x: gameBoard.width / 2,
+    y: gameBoard.height / 2
+  },
+  {
+    x: gameBoard.width / 2 - 10,
+    y: gameBoard.height / 2 - 10
+  },
+  {
+    x: gameBoard.width / 2 - 20,
+    y: gameBoard.width / 2 - 20
+  },
+];
 
 
 main();
@@ -39,81 +61,90 @@ main();
 gen_food();
 
 
-function restart(){
+function restart() {
   scoreBoard.innerHTML = `Score: 0`;
   score = 0;
   snake = Array.from(snakeInnit);
   gen_food();
-  if(over){
+  if (over) {
     over = false;
     main();
   }
 
- }
-
-function drawSnakePart(part){
-    gameContext.fillStyle = 'white';  
-    gameContext.strokestyle = 'black';
-    gameContext.fillRect(part.x, part.y, 10,10);
-    gameContext.strokeRect(part.x, part.y, 10,10);
 }
 
-function drawFood(){
-  gameContext.fillStyle = 'red';  
+function resume() {
+  paused = false;
+  main();
+}
+
+function drawSnakePart(part) {
+  gameContext.fillStyle = 'white';
+  gameContext.strokestyle = 'black';
+  gameContext.fillRect(part.x, part.y, 10, 10);
+  gameContext.strokeRect(part.x, part.y, 10, 10);
+}
+
+function drawFood() {
+  gameContext.fillStyle = 'red';
   gameContext.strokestyle = 'lightred';
-  gameContext.fillRect(food_x, food_y, 10,10);
-  gameContext.strokeRect(food_x, food_y, 10,10);
+  gameContext.fillRect(food_x, food_y, 10, 10);
+  gameContext.strokeRect(food_x, food_y, 10, 10);
 }
 
 
 
 
 function clearCanvas() {
-      
-    gameContext.fillStyle = '#2196f3';
-    gameContext.strokestyle = 'black';
-    gameContext.fillRect(0, 0, gameBoard.width, gameBoard.height );
-     gameContext.strokeRect(0, 0, gameBoard.width, gameBoard.height);
-      }
 
-function drawSnake(){
-    snake.forEach(drawSnakePart)
+  gameContext.fillStyle = '#2196f3';
+  gameContext.strokestyle = 'black';
+  gameContext.fillRect(0, 0, gameBoard.width, gameBoard.height);
+  gameContext.strokeRect(0, 0, gameBoard.width, gameBoard.height);
 }
 
-function moveSnake() 
-{  
-   const head = {x: snake[0].x + dx, y: snake[0].y + dy};
+function drawSnake() {
+  snake.forEach(drawSnakePart)
+}
+
+function moveSnake() {
+  const head = {
+    x: snake[0].x + dx,
+    y: snake[0].y + dy
+  };
   snake.unshift(head);
 
-const collided = snake[0].x === food_x && 
-snake[0].y === food_y;
+  const collided = snake[0].x === food_x &&
+    snake[0].y === food_y;
 
-if(collided){
+  if (collided) {
 
-  score += 10;
+    score += 10;
 
-gen_food();
-scoreBoard.innerHTML = `Score: `+ score;
-} else {
+    gen_food();
+    scoreBoard.innerHTML = `Score: ` + score;
+  } else {
 
-  snake.pop();
+    snake.pop();
+  }
 }
-}
 
-function main() 
-{  
+function main() {
 
+  while (paused) {
+    main();
+  }
 
-  if(gameOver()){
+  if (gameOver()) {
     over = true;
     highScoreArray.push(score);
-   
-    let scoreListHTML = ``; 
-    highScoreArray.forEach( element =>  {
-      scoreListHTML += `<li> ${element} </li>`; 
+
+    let scoreListHTML = ``;
+    highScoreArray.forEach(element => {
+      scoreListHTML += `<li> ${element} </li>`;
     });
-    
-    
+
+
     scoreList.innerHTML = scoreListHTML;
     scoreBoard.innerHTML = `Score: 0`;
     score = 0;
@@ -121,100 +152,93 @@ function main()
   }
 
 
-   setTimeout(function onTick() 
-   {    
-     clearCanvas();    
-     drawFood();
-     moveSnake();  
-     drawSnake();
-     
-     main();
-   }, 200)
+  setTimeout(function onTick() {
+    clearCanvas();
+    drawFood();
+    moveSnake();
+    drawSnake();
+
+    main();
+  }, 200)
 }
 
 
-function snakeControl(event){
+function snakeControl(event) {
 
   const A_KEY = 65;
   const W_KEY = 87;
   const S_KEY = 83;
   const D_KEY = 68;
 
-const keyPressed = event.keyCode;
+  const keyPressed = event.keyCode;
 
-const goLeft = dx === -10;
-const goRight = dx === 10;
-const goUp = dy === -10;
-const goDown = dy === 10;
+  const goLeft = dx === -10;
+  const goRight = dx === 10;
+  const goUp = dy === -10;
+  const goDown = dy === 10;
 
-if (keyPressed === A_KEY && !goRight)
-{
-  dx = -10;
-  dy = 0;
-}
+  if (keyPressed === A_KEY && !goRight) {
+    dx = -10;
+    dy = 0;
+  }
 
-if (keyPressed === W_KEY && !goDown)
-{
-  dx = 0;
-  dy = -10;
-}
+  if (keyPressed === W_KEY && !goDown) {
+    dx = 0;
+    dy = -10;
+  }
 
-if(keyPressed === S_KEY && !goUp)
-{
-  dx = 0;
-  dy = 10;
-}
+  if (keyPressed === S_KEY && !goUp) {
+    dx = 0;
+    dy = 10;
+  }
 
-if(keyPressed === D_KEY && !goLeft)
-{
-  dx = 10;
-  dy = 0;
-}
+  if (keyPressed === D_KEY && !goLeft) {
+    dx = 10;
+    dy = 0;
+  }
 
 }
 
 document.addEventListener('keydown', snakeControl);
 
-function gameOver(){
+function gameOver() {
 
-  for(let c = 3; c < snake.length; c++)
-  {
-  const collided = snake[c].x === snake[0].x && 
-  snake[c].y === snake[0].y;
+  for (let c = 3; c < snake.length; c++) {
+    const collided = snake[c].x === snake[0].x &&
+      snake[c].y === snake[0].y;
 
-  if(collided)
-  {
-  return true;
+    if (collided) {
+      return true;
+    }
   }
-}
 
   const hitLeft = snake[0].x < 0;
-  const hitRight = snake[0].x > gameBoard.width- 10;
+  const hitRight = snake[0].x > gameBoard.width - 10;
   const hitTop = snake[0].y < 0;
-  const hitBottom = snake[0].y > gameBoard.height-10;
+  const hitBottom = snake[0].y > gameBoard.height - 10;
 
   return hitLeft || hitRight || hitTop || hitBottom;
 
-  }
+}
 
 
-  function food_coord(min, max){
-return Math.round((Math.random() * (max - min) + min) / 10 ) * 10;
+function food_coord(min, max) {
+  return Math.round((Math.random() * (max - min) + min) / 10) * 10;
 
-  }
+}
 
-  function gen_food(){
+function gen_food() {
 
   food_x = food_coord(0, gameBoard.width - 10);
   food_y = food_coord(0, gameBoard.height - 10);
 
-  snake.forEach( part => {
-const eaten = part.x == food_x && part.y == food_y;
-if(eaten) {
-  gen_food();
-}
+  snake.forEach(part => {
+    const eaten = part.x == food_x && part.y == food_y;
+    if (eaten) {
+      gen_food();
+    }
 
   })
 
 
-  }
+}
